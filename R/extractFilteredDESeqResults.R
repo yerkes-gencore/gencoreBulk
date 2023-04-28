11b5ea8205ab00f1af67aea960be1f6aea0cfc83
Derrik Gratz <derrik.gratz@gmail.com>
2023-04-28 17:05:23 -0400

    add filtered results extraction functions

---
+++ R/extractFilteredDESeqResults.R
@@ -0,0 +1,84 @@
+#' Extract results from processed DESeq2 object using Greg's filtering
+#'
+#' Calls DESeq2::results with the filter specified using Greg's
+#' minMaxFilter function. Greg's filter dynamically filters results for 
+#' minimum supporting reads based on the samples involved in the result calculation.
+#' This function is intended to reduce the
+#' overall code used to extract all results of interest.
+#' 
+#' @rdname extractFilteredDeseqResults
+#' @param comparison Value to pass to [DESeq2::results()] as either a `contrast`
+#'  or a `name`
+#' @param intgroup Metadata column on which to draw groups for filtering
+#' @param filt_groups Character vector of groups to draw from `intgroup`
+#' @param test Test to use, default is `Wald`
+#' @param dds DESeq2 object to draw results from
+#' @param alpha The significance cutoff used for optimizing the independent 
+#'  filtering (by default 0.05). If the adjusted p-value cutoff (FDR) will be a 
+#'  value other than 0.05, alpha should be set to that value.
+#' @param ... Additional arguments passed to [DESeq2::results()]
+#' 
+#' @returns object of class DESeqResults
+#' @import DESeq2
+#' 
+#' @export
+
+extractFilteredDESeqResults <- function(comparison, 
+                                        intgroup,
+                                        filt_groups,
+                                        test = 'Wald',
+                                        dds = analysis$dds,
+                                        alpha=0.05,
+                                        ...){
+  if (class(comparison) == 'character' & length(comparison) == 1){
+    result <- results(dds,
+                      name=comparison, 
+                      test=test,
+                      alpha=alpha,
+                      ...,
+                      filter = maxMinFilter(dds,
+                                            intgroup = intgroup,
+                                            comp = filt_groups))  
+  } else {
+    result <- results(dds,
+                      contrast=comparison, 
+                      test=test,
+                      alpha=alpha,
+                      ...,
+                      filter = maxMinFilter(dds,
+                                            intgroup = intgroup,
+                                            comp = filt_groups))
+  }
+  result@metadata$contrast <- paste0(unlist(comparison), collapse='_')
+  return(result)
+}
+
+
+.maxMinFilter <- function(object,
+                          intgroup,
+                          comp, 
+                          thresh = 0)
+{
+  if (!all(intgroup %in% names(colData(object)))) {
+    stop("the argument 'intgroup' should specify columns of colData(dds)")
+  }
+  intgroup.df <- as.data.frame(colData(object)[, intgroup, drop = FALSE])
+  
+  # group <- if (length(intgroup) > 1) {
+  #   interaction(lapply(intgroup, function(factorname) colData(ddsMoTime20034)[[factorname]]), drop = TRUE)
+  #   #    factor(apply(intgroup.df, 1, paste, collapse = " : "))
+  # }
+  # else {
+  group <- colData(object)[[intgroup]]
+  # }
+  if (!all(comp %in% levels(group))) {
+    stop("the argument 'comp' should specify levels of intgroup")
+  }
+  if_else(
+    rowMaxs(
+      sapply(comp, function(lvl) {
+        rowMins(counts(object, normalize = TRUE)[,group == lvl])
+      })) > thresh,rowMeans2(counts(object, normalized = TRUE)
+      ), 0
+  )
+}
