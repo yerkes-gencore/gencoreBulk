f7f2d0f3d7c5be873fe74422d08c7aa9ee5fdb5d
Derrik Gratz <derrik.gratz@gmail.com>
2023-05-01 14:45:37 -0400

    expand dependancies, explicit namespaces for functions

--- R/RLE.R
+++ R/RLE.R
@@ -3,32 +3,38 @@
 #' Returns plots of count frequencies before and after normalization
 #' 
 #' @rdname checkRLE
-#' @param raw_counts matrix of raw count data
 #' @param analysis object with config options saved from the template format
 #'    
 #' @returns 2 ggplots
 #'
 #' @examples
+#' \dontrun{
 #'   checkRLE(raw_counts, analysis)
+#' }
+#'   
 #' 
-#' @importFrom DESeq2 counts
+#' @importFrom DESeq2 counts 
+#' @importFrom SummarizedExperiment assays
 #' @importFrom tibble as_tibble
+#' @importFrom forcats fct_inorder
+#' @importFrom tidyr pivot_longer
+#' @importFrom matrixStats rowMedians rowMins
 #' @import ggplot2
 #' 
 #' @export
 
 checkRLE <- function(analysis){
-  raw_counts <- assays(analysis$dds)$counts
+  raw_counts <- SummarizedExperiment::assays(analysis$dds)$counts
   rawLogCounts <- log(raw_counts[rowMins(raw_counts)>0,])
-  rawMedianLogs <- rowMedians(rawLogCounts)
+  rawMedianLogs <- matrixStats::rowMedians(rawLogCounts)
   rawLogRatios <- rawLogCounts - rawMedianLogs
   
   
   normLogCounts <- log(
     DESeq2::counts(analysis$dds, normalized = TRUE)[
-      rowMins(DESeq2::counts(analysis$dds))>0,]
+      matrixStats::rowMins(DESeq2::counts(analysis$dds))>0,]
   )
-  normMedianLogs <- rowMedians(normLogCounts)
+  normMedianLogs <- matrixStats::rowMedians(normLogCounts)
   normLogRatios <- normLogCounts - normMedianLogs
   RLE_raw <- .plotRLE(rawLogRatios, 'RLE Raw')
   RLE_norm <- .plotRLE(normLogRatios, 'RLE Normalized')
@@ -36,13 +42,14 @@ checkRLE <- function(analysis){
 }
 
 .plotRLE <- function(data, title){
-  as_tibble(data, rownames = NA) %>% 
-    pivot_longer(everything(), names_to = "Sample", values_to = "RLE") %>% 
-    ggplot(aes(x=Sample,y=RLE)) +
-    geom_hline(yintercept = 0, color = "red") + geom_violin(draw_quantiles = c(0.25,0.75), trim = TRUE, color = "lightgreen", alpha = 0.1) +
+  tibble::as_tibble(data, rownames = NA) %>% 
+    tidyr::pivot_longer(everything(), names_to = "Sample", values_to = "RLE") %>% 
+    ggplot(aes(x=.data$Sample,y=.data$RLE)) +
+    geom_hline(yintercept = 0, color = "red") +
+    geom_violin(draw_quantiles = c(0.25,0.75), trim = TRUE, color = "lightgreen", alpha = 0.1) +
     geom_boxplot(alpha = 0) + 
     theme_bw() +
-    theme(axis.text.x = element_text(vjust = 0.5,angle = 90),axis.title.x = element_blank(),aspect.ratio = 0.55) + 
+    theme(axis.text.x = element_text(vjust = 0.5,angle = 90), axis.title.x = element_blank(),aspect.ratio = 0.55) + 
     ggtitle(title) + 
-    aes(fct_inorder(Sample))#+ scale_y_continuous(limits = c(-9,3.25), expand = c(0,0))
+    aes(forcats::fct_inorder(.data$Sample))#+ scale_y_continuous(limits = c(-9,3.25), expand = c(0,0))
 }
