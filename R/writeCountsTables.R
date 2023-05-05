7eb2dd1c870506dcbae332f84696bb18b714ddde
Derrik Gratz <derrik.gratz@gmail.com>
2023-05-05 10:47:13 -0400

    option to write normalized counts out, and option to round them to n digits

--- R/writeCountsTables.R
+++ R/writeCountsTables.R
@@ -4,6 +4,8 @@
 #'  options saved to write data to named file outputs.
 #'
 #' @param analysis object with config options saved from the template format
+#' @param normalized Whether to write normalized or raw counts
+#' @param normalized_digits Number of digits to write out for normalized counts
 #' @param outdir folder to store outputs in, will be generated if it doesn't exist
 #' @param generate_GSEA_app_tables write files for compatibility with GSEA desktop app
 #' @param write_sample_table write sample metadata to a csv
@@ -18,25 +20,31 @@
 #' @importFrom readr write_csv write_tsv write_lines
 #' @importFrom tibble rownames_to_column
 #' @importFrom stringr str_remove
+#' @importFrom DESeq2 counts
+#' @import SummarizedExperiment
 #'
 #' @export
 
 
 writeCountTables <- function(analysis,
+                             normalized = FALSE,
+                             normalized_digits = 10,
                              outdir = here::here("outputs"),
                              generate_GSEA_app_tables = FALSE,
                              write_sample_table = TRUE) {
   if (!dir.exists(outdir)) {
     dir.create("outputs")
   }
-  ## raw counts w/ gene symbols
-  assays(analysis$dds)$counts %>%
+  ## counts w/ gene symbols
+  DESeq2::counts(analysis$dds, normalized = normalized) %>%
     as.data.frame() %>%
+    round(digits = normalized_digits) %>%
     tibble::rownames_to_column(var = "gene_id") %>%
     readr::write_csv(
       file = here::here(paste0(
-        outdir, "/raw_count_",
-        analysis$config$analysis, ".csv"
+        outdir, 
+        (if (normalized){'/normalized_counts_'} else{"/raw_count_"}),
+        analysis$qc_config$analysis, ".csv"
       )),
       col_names = TRUE
     )
@@ -44,7 +52,7 @@ writeCountTables <- function(analysis,
     readr::write_csv(analysis$sampleTable,
       file = here::here(paste0(
         outdir, "/sample_table_",
-        analysis$config$analysis, ".csv"
+        analysis$qc_config$analysis, ".csv"
       ))
     )
   }
@@ -70,7 +78,7 @@ writeCountTables <- function(analysis,
     readr::write_lines(analysis$clsLinesGroup,
       file = here::here(paste0(
         outdir, "/Group_",
-        analysis$config$analysis, ".cls"
+        analysis$qc_config$analysis, ".cls"
       ))
     )
     readr::write_tsv(
@@ -80,7 +88,7 @@ writeCountTables <- function(analysis,
       ),
       file = here::here(paste0(
         outdir, "/rlog_forGSEA_",
-        analysis$config$analysis, ".txt"
+        analysis$qc_config$analysis, ".txt"
       ))
     )
   }
