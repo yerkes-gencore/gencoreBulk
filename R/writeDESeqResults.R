6f63d237a55b381293ac7c422a515add2fee922f
Derrik Gratz <derrik.gratz@gmail.com>
2023-08-31 09:42:47 -0400

    namespace saveworkbook

--- R/writeDESeqResults.R
+++ R/writeDESeqResults.R
@@ -45,13 +45,13 @@ writeDESeqResults <- function(results,
   outfile <- file.path(outdir, output_name)
   message(paste0('Writing results to ', outfile))
   wb <- openxlsx::createWorkbook('ENPRC Gencore')
-  mapply(FUN=.addWorksheet_DESeqres,
+  mapply(FUN = .addWorksheet_DESeqres,
          result=results,
          sheet_name=sheet_names,
          MoreArgs = list(wb=wb,
                          drop_NA=drop_NA)
   )
-  saveWorkbook(wb, outfile, overwrite = TRUE)
+  openxlsx::saveWorkbook(wb, outfile, overwrite = TRUE)
 }
 
 .addWorksheet_DESeqres <- function(wb,
