5b587ce3491ce7c7432556e9637881691f16ef7d
derrik-gratz <derrik.gratz@gmail.com>
2024-02-22 19:41:51 +0000

    init plotModelCoeffs

--- R/plotGeneExpression.R
+++ R/plotGeneExpression.R
@@ -5,11 +5,17 @@
 #'  aesthetics. This is meant to be minimal so you can adjust the aesthetics
 #'  as necessary, but it can still be used as a quick check. 
 #'  
+#' You can subset data using the `subsetting` and `subsets` arguments to focus
+#'  the plotting.
+#'
+#'  
 #' @param gene Gene to plot expression for
 #' @param counts The expression data to plot
 #' @param metadata The metadata to associate with the counts data
 #' @param grouping Variable to group expression by. Must be a column of `metadata`
 #' @param groups Optional specification of level ordering for grouping var
+#' @param subsetting Optional variable to subset expression by. Must be a column of `metadata`
+#' @param subsets Specification of levels to subset for
 #' @param boxplot Boolean, plot `geom_boxplot`?
 #' @param jitter Boolean, plot `geom_jitter`?
 #' @param axis_text_x Option to modify X axis text. Default rotates labels 90
@@ -25,11 +31,12 @@
 #' \dontrun{
 #' 
 #'   ## With a limma model fit
-#'   plotGeneExpression('MAMU-A', counts = model_fit$EList$E, metadata = model_fit$targets, grouping = 'groupIDs')
+#'   plotGeneExpression('MAMU-A', counts = model_fit$EList$E,
+#'                       metadata = model_fit$targets, grouping = 'groupIDs')
 #'   
 #'   ## With a DESeq model
 #'   plotGeneExpression('MAMU-A', counts = assay(assays(analysis$dds)$rld), 
-#'                      metadata = colData(analysis$dds), grouping = 'groupIDs')
+#'                       metadata = colData(analysis$dds), grouping = 'groupIDs')
 #' }
 #' 
 plotGeneExpression <- function(gene,
@@ -37,6 +44,8 @@ plotGeneExpression <- function(gene,
                                groups,
                                counts,
                                metadata,
+                               subsetting,
+                               subsets,
                                boxplot = TRUE,
                                jitter = TRUE,
                                axis_text_x = element_text(angle = 90, vjust = 0.5, hjust=1)) {
@@ -48,6 +57,20 @@ plotGeneExpression <- function(gene,
   }
   if (!grouping %in% colnames(metadata)) {
     stop('grouping variable not found in metadata')
+  }
+  if (xor(missing(subsetting), missing(subsets))) {
+    stop('Subsetting data requires both subsets and subsetting to be specified')
+  } 
+  if (any(colnames(counts) != rownames(metadata))) {
+    warning('Colnames of counts does not match rownames of metadata. Continuing, but
+            this may suggest the data are not properly associated')
+  }
+  if (!missing(subsetting)) {
+    if (!subsetting %in% colnames(metadata)) {
+      stop('subsetting variable not found in metadata')
+    }
+    metadata <- metadata[metadata[[subsetting]] %in% subsets,]
+    counts <- counts[,rownames(metadata)]
   }
   plotdata <- utils::stack(counts[gene, ])
   colnames(plotdata) <- 'Gene'
@@ -64,4 +87,113 @@ plotGeneExpression <- function(gene,
     ggplot2::theme_bw() +
     ggplot2::labs(x = 'Group', y = 'Expression', main = gene) +
     ggplot2::theme(axis.text.x = axis_text_x)
+}
+
+
+
+
+#' Plot the results of a model fit 
+#' 
+#' Plots the expression, coefficients, and estimated change for a single gene and
+#' contrast. You have to pass in a contrasts dataframe (or list) and specify which 
+#' contrast to use. See the example for details. The rest of the arguments get 
+#' passed to `plotGeneExpression`. 
+#' 
+#' For example, if the question is:
+#'  "the results say JUNB has a significant increase in expression,
+#'   in Day 14 compared to day 0, does this seem to be true?"
+#' 
+#' Your contrast list/dataframe would look something like this:
+#' 
+#' | contrast_name |       numerators         |  denominators |
+#' |---------------|--------------------------|---------------|
+#' | Day14_v_Day0  | '(Intercept) + dayDay14' | '(Intercept)' |
+#' 
+#' You would specify your contrast as 'Day14_v_Day0'
+#'
+#' @param gene Gene to plot
+#' @param contrast Name of contrast, must be present in `contrasts`
+#' @param coefficients Matrix of coefficients fit by model of your choice
+#' @param contrasts Dataframe of contrasts, with columns for contrast name, 
+#'  numerator, and denominator
+#' @inheritParams plotGeneExpression
+#'
+#' @returns A ggplot object
+#' @export
+#' 
+#' @import ggplot2
+#'
+#' @examples 
+#' \dontrun{ 
+#' 
+#' contrast_matrix_expanded <- data.frame(
+#'   contrast_names = 'P14_grp3vgrp2',
+#'   numerators = '(Intercept) + grpgrp3',
+#'   denominators = '(Intercept)')
+#'   
+#' model_plot('JUN', contrast = 'P14_grp3vgrp2',
+#'              counts = model_fit$EList$E, metadata = model_fit$targets,
+#'              grouping = 'grp.day',
+#'              subsetting = 'grp.day', subsets = c('grp2.P14', 'grp3.P14'),
+#'              coefficients = limma_fit$fit$coefficients,
+#'              contrasts = contrast_matrix_expanded)
+#' }
+plotModelCoeffs <- function(gene, contrast, 
+                       counts, metadata, 
+                       grouping, groups, 
+                       subsetting, subsets,
+                       coefficients, contrasts) { 
+  baseplot <- plotGeneExpression(gene,
+                                 counts =  counts,
+                                 metadata = metadata,
+                                 grouping = grouping,
+                                 groups = groups,
+                                 subsetting = subsetting,
+                                 subsets = subsets)
+  contrast_coeffs <- contrasts[contrasts$contrast_names == contrast,]
+  contrast_coeffs <- .extract_coefficients(gene = gene, 
+                                          contrast = contrast_coeffs, 
+                                          coefficients = coefficients)
+  contrast_num <- round(contrast_coeffs$numerator, 2)
+  contrast_den <- round(contrast_coeffs$denominator, 2)
+  contrast_line_num <- ggplot2::geom_hline(yintercept = contrast_num, linetype = 'dashed') 
+  contrast_line_den <- ggplot2::geom_hline(yintercept = contrast_den, linetype = 'dashed') 
+  
+  arrow <- ggplot2::geom_segment(aes(x = 1.5, xend = 1.5, 
+                            y = contrast_den, 
+                            yend = contrast_num),
+                        arrow = ggplot2::arrow()) 
+  baseplot + 
+    contrast_line_num +
+    contrast_line_den +
+    arrow +
+    ggplot2::scale_y_continuous(breaks = c(contrast_den, contrast_num))
+}
+
+.extract_coefficients <- function(gene,
+                                  coefficients,
+                                  contrast) {
+  if (!gene %in% rownames(coefficients)) {
+    stop('Gene not found in coefficients')
+  }
+  coefficients <- coefficients[gene,]
+  numerator_terms <- unlist(strsplit(contrast$numerators, '\\s*\\+\\s*', perl = TRUE))
+  numerator <- 0
+  for (term in numerator_terms) {
+    if (!term %in% names(coefficients)) {
+      stop(paste0("Error in numerator specification. Term '", term,
+                  "' not found in matrix of coefficients"))
+    }
+    numerator <- numerator + unname(coefficients[term])
+  }
+  denominator_terms <- unlist(strsplit(contrast$denominators, '\\s*\\+\\s*', perl = TRUE))
+  denominator <- 0
+  for (term in denominator_terms) {
+    if (!term %in% names(coefficients)) {
+      stop(paste0("Error in denominator specification. Term '", term,
+                  "' not found in matrix of coefficients"))
+    }
+    denominator <- denominator + unname(coefficients[term])
+  }
+  return(list(contrast_name = contrast$contrast_names, numerator = numerator, denominator = denominator))
 }
