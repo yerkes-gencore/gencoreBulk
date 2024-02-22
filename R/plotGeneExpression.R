bef6449799443ea8f6722ec3277d282272a42bd2
derrik-gratz <derrik.gratz@gmail.com>
2024-02-22 15:50:23 +0000

    generalize gene expression plot for differnt input

--- R/plotGeneExpression.R
+++ R/plotGeneExpression.R
@@ -6,9 +6,10 @@
 #'  as necessary, but it can still be used as a quick check. 
 #'  
 #' @param gene Gene to plot expression for
-#' @param grouping Variable to group expression by. Must be a column of `data@colData`
-#' @param data A DESeqDataSet object. Use this argument to select specific assays
-#'  to plot. The default is `assays(analysis$dds)$rld` (assumed to be the rlog transform).
+#' @param counts The expression data to plot
+#' @param metadata The metadata to associate with the counts data
+#' @param grouping Variable to group expression by. Must be a column of `metadata`
+#' @param groups 
 #' @param boxplot Boolean, plot `geom_boxplot`?
 #' @param jitter Boolean, plot `geom_jitter`?
 #' @param axis_text_x Option to modify X axis text. Default rotates labels 90
@@ -18,30 +19,45 @@
 #' @export
 #'
 #' @import ggplot2
-#' @importFrom SummarizedExperiment assay
 #' @importFrom rlang .data
+#' @importFrom utils stack
 #' @examples
 #' \dontrun{
-#' ## my object is called dds_full, so I have to manually provide data
-#' plotGeneExpression('MAMU-A', grouping = 'groupIDs', data = assays(dds_full)$rld)
+#' 
+#'   ## With a limma model fit
+#'   plotGeneExpression('MAMU-A', counts = model_fit$EList$E, metadata = model_fit$targets, grouping = 'groupIDs')
 #' }
 #' 
 plotGeneExpression <- function(gene,
                                grouping,
-                               data = SummarizedExperiment::assays(analysis$dds)$rld,
+                               groups,
+                               counts,
+                               metadata,
                                boxplot = TRUE,
                                jitter = TRUE,
                                axis_text_x = element_text(angle = 90, vjust = 0.5, hjust=1)) {
-  if (!(gene %in% rownames(data))){
-    stop('Gene not found in data')
+  # if (missing(gene) | missing(grouping) | missing(counts) | missing(metadata)) {
+  #   stop('The following arguments are all required: gene, grouping, groups, counts, metadata')
+  # }
+  if (!(gene %in% rownames(counts))){
+    stop('Gene not found in counts data')
   }
-  plotdata <- as.data.frame(t(SummarizedExperiment::assay(data[gene, ])))
+  if (!grouping %in% colnames(metadata)) {
+    stop('grouping variable not found in metadata')
+  }
+  plotdata <- utils::stack(counts[gene, ])
   colnames(plotdata) <- 'Gene'
-  plotdata$grouping <- data@colData[[grouping]]
+  plotdata$grouping <- metadata[[grouping]]
+  if (!missing(groups)) {
+    if (!all(groups %in% unique(metadata[[grouping]]))) {
+      stop('All groups not found in grouping variable of metadata')
+    }
+    plotdata$grouping <- factor(plotdata$grouping, levels = groups)
+  }
   ggplot2::ggplot(plotdata, aes(x=grouping, y=.data[['Gene']])) + 
     (if (boxplot) { ggplot2::geom_boxplot(outlier.color = if (jitter) {NA} else {'black'}) }) +
-    ggplot2::theme_bw() +
     (if (jitter) { ggplot2::geom_jitter() }) +
+    ggplot2::theme_bw() +
     ggplot2::labs(x = 'Group', y = 'Expression', main = gene) +
     ggplot2::theme(axis.text.x = axis_text_x)
 }
