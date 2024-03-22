b8023a8d9daec4da9c3c70a7e2bd698ef12158df
derrik-gratz <derrik.gratz@gmail.com>
2024-03-22 17:03:28 +0000

    rm redundant .plotRLE

--- R/RLE.R
+++ R/RLE.R
@@ -54,16 +54,3 @@ checkRLE <- function(dds) {
     ggtitle(title) +
     aes(forcats::fct_inorder(.data$Sample)) #+ scale_y_continuous(limits = c(-9,3.25), expand = c(0,0))
 }
-
-.plotRLE <- function(data, title) {
-  tibble::as_tibble(data, rownames = NA) %>%
-    tidyr::pivot_longer(everything(), names_to = "Sample", values_to = "RLE") %>%
-    ggplot(aes(x = .data$Sample, y = .data$RLE)) +
-    geom_hline(yintercept = 0, color = "red") +
-    geom_violin(draw_quantiles = c(0.25, 0.75), trim = TRUE, color = "lightgreen", alpha = 0.1) +
-    geom_boxplot(alpha = 0) +
-    theme_bw() +
-    theme(axis.text.x = element_text(vjust = 0.5, angle = 90), axis.title.x = element_blank(), aspect.ratio = 0.55) +
-    ggtitle(title) +
-    aes(forcats::fct_inorder(.data$Sample)) #+ scale_y_continuous(limits = c(-9,3.25), expand = c(0,0))
-}
